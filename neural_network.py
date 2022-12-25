a7c4840b881156410002f7d889741f6d0f00b792
sabitova-fatima <fatima.sabitova@genestack.com>
2022-12-25 17:10:55 +0100

    base class implented

---
+++ neural_network.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# the initial class we will inherit from
+class Layer:
+	def __init__(self):
+		self.input = None
+		self.output = None
+	def forward(self, input):
+		pass
+	def backward(self, output_gradient, learning_rate):
+		pass
+
